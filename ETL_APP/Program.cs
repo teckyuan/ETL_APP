0998fd92df633fbe98f79d73af06011ed2404c65
teckyuan <teckyuan9898@hotmail.com>
2019-01-25 13:25:43 +0800

    Add project files.

---
+++ ETL_APP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ETL_APP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
