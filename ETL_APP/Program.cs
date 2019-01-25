9f9b33191bf7a7c62fd0eded8975d204ce3b4611
teckyuan <teckyuan9898@hotmail.com>
2019-01-25 15:09:34 +0800

    read string from web

--- ETL_APP/Program.cs
+++ ETL_APP/Program.cs
@@ -1,23 +1,48 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ETL_APP
 {
     class Program
     {
+        static IConfiguration config;
+
         static void Main(string[] args)
         {
+            //load from config
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json");
 
-            IConfiguration config = new ConfigurationBuilder()
+            config = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", true, true)
                 .Build();
 
             //test
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($" Hello { config["url"] } !");
+
+            //download webpage from url
+            Task t = new Task(DownloadPageAsync);
+            t.Start();
+            Console.WriteLine("Downloading page...");
+            Console.ReadLine();
+        }
+
+        static async void DownloadPageAsync()
+        {
+            string url = config["url"];
+
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.GetAsync(url))
+            using (HttpContent content = response.Content)
+            {
+                string result = await content.ReadAsStringAsync();
+                
+                //Console.WriteLine(result);
+            }
         }
     }
 }
